254df10332063ec9d6a233feb732fd276d2c3d0b
Khamitov02 <78606737+Khamitov02@users.noreply.github.com>
2021-05-07 21:00:07 +0300

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/nibbles_01.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/nibbles_01.dir/src/main.cpp.obj"
+  "CMakeFiles/nibbles_01.dir/src/menu.cpp.obj"
+  "libnibbles_01.dll.a"
+  "nibbles_01.exe"
+  "nibbles_01.exe.manifest"
+  "nibbles_01.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/nibbles_01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
